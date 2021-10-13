986d16f1329403cfa13cb627d899822ddef32620
Ritik Kumar <kumarritik@microsoft.com>
2021-10-13 22:47:42 +0530

    Session Auth 2

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const MongoDBSession = require('connect-mongodb-session')(session);
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const UserModel = require('./Model/User');
 
@@ -35,11 +37,16 @@ app.use(session({
     store: store
 }));
 
-app.get('/', (req, res) => {
-    req.session.isAuth = true;
-    console.log(req.session);
-    console.log(req.session.id);
+const isAuth = (req, res, next) => {
+    if(req.session.isAuth) {
+        next();
+    }
+    else {
+        return res.redirect('/login');
+    }
+}
 
+app.get('/', (req, res) => {
     res.send("Welcome to our app");
 });
 
@@ -51,31 +58,99 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
 
+    let user = await UserModel.findOne({email});  // database -> search -> return
+
+    if(!user) {
+        return res.send("User not found");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+        return res.send("Invalid Password");
+    }
+
+    req.session.isAuth = true;
+    console.log(req.session);
+    console.log(req.session.id);
+
+    res.redirect('/dashboard');
+    return;
 });
+
+const cleanAndValidate = ({name, email, password}) => {
+
+    return new Promise(async (resolve, reject) => {
+        if(typeof(email) != "string") email = "";
+        if(typeof(password) != "string") password = ""; 
+
+        if(!email || !password) 
+            reject("Invalid Data Provided");
+
+        if(!validator.isEmail(email)) 
+            reject("Invalid Email");
+
+        if(password.length > 0 && password.length < 6) // 6 character, small, capital, symbol, number
+             reject("Password too short");
+
+        let user = await UserModel.findOne({email});
+
+        if(user)
+            reject("User already exists");
+
+        resolve();
+    });  
+
+}
 
 app.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
-
-    let user = await UserModel.findOne({email});
-
-    if(user) {
-        res.send("User already exists");
+    try {
+        await cleanAndValidate(req.body);
+        await find();
+        await game();
+    }
+    catch(err) {
+        console.log(err);
+        res.send(err);
         return;
     }
+
+    const hashedPassword = await bcrypt.hash(password, 13); 
 
     user = new UserModel({
         name,
         email,
-        password
+        password: hashedPassword
     });
 
     user.save();
 
-    res.send('Registration successfull');
+    res.redirect('/login');
+});
+
+app.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if(err) throw err;
+
+        res.redirect('/');
+    });
+})
+
+app.get('/dashboard', isAuth, (req, res) => {
+    res.render('dashboard');
+});
+
+app.get('/home', isAuth, (req, res) => {
+    res.send('Welcome to home page');
 });
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
+
+// Route, Middleware
